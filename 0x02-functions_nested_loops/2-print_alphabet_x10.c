599d2cec4a4b882a9d85830025e0ba03a221978f
Kkarabo778 <kkaraboannah778@gmail.com>
2022-10-18 10:40:40 -0700

    K

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,6 +12,6 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-			putchar(letter);
-		_putchar('\n')
+			_putchar(letter);
+		_putchar('\n');
 	}
