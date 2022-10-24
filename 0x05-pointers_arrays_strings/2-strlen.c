f1331dff804faff2a837efd1df87c113b48795d8
Kkarabo778 <kkaraboannah778@gmail.com>
2022-10-24 15:02:03 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @s: String pointer to the string whose length is to be found
+ * Return: returns the lenth of the string.
+ */
+
+int _strlen(char *s)
+{
+	int p = 0;
+	/*increament upto when the last character is NULL,\0*/
+	while (*(s + p) ! = 0)
+	{
+		p++;
+	}
+	return (p);
+}
